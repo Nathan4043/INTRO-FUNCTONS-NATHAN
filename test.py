4063dd1b0223c3c57478ebf862233fb8dd939088
Nathan4043 <nathanr88@nycstudents.net>
2025-09-10 09:49:59 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,36 @@
 import turtle
 from turtle import *
 t = Turtle()
-
-
 t.shape('turtle')
+t.speed(1.5)
+t.forward(100)
+t.left(90)
+t.forward(125)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(125)
+turtle.clearscreen()
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+t.speed(1.5)
+t.forward(90)
+t.left(120)
+t.forward(90)
+t.left(120)
+t.forward(90)
 
-t.forward(200)
-lioasdjfhgiop;afk
+
+
+
+
+
+
+
+
+
+
+
 turtle.done()
