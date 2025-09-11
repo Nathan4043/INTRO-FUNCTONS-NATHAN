b0f0a50c8d96ecb5bbd70912d88cf7b0ce1ca70e
Nathan4043 <nathanr88@nycstudents.net>
2025-09-11 07:16:02 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-t.speed(1.5)
+t.speed(3)
 t.forward(100)
 t.left(90)
 t.forward(125)
@@ -15,19 +15,26 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-t.speed(1.5)
+t.speed(3)
 t.forward(90)
 t.left(120)
 t.forward(90)
 t.left(120)
 t.forward(90)
-
-
-
-
-
-
-
+turtle.clearscreen()
+import turtle
+from turtle import *
+t = Turtle()
+t.speed(1000)
+for i in range(60):
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(5)
 
 
 
