f1b0682416efb11c1f30ebaf07eacfa5f0b2ba8a
Nathan4043 <nathanr88@nycstudents.net>
2025-09-12 07:16:13 -0700

    Update test.py

--- test.py
+++ test.py
@@ -25,6 +25,7 @@ turtle.clearscreen()
 import turtle
 from turtle import *
 t = Turtle()
+t.shape('turtle')
 t.speed(1000)
 for i in range(60):
     t.forward(100)
@@ -40,8 +41,29 @@ turtle.clearscreen()
 import turtle
 from turtle import *
 t = Turtle()
-
-
+t.shape('turtle')
+t.speed(99999999999999999999999999999999)
+length=5
+Turn=90
+for i in range(60):
+    forward (length)
+    left(Turn)
+    forward (length)
+    left(Turn)
+    forward(length)
+    left(Turn)
+    forward(length)
+    left(Turn)
+    right(5)
+    length+=5
+turtle.clearscreen()
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+for i in range(5):
+    forward(100)
+    left(144)
 
 
 
