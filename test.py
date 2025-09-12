e0fc702654dec24c3f028c3be9e927227f1fe134
Nathan4043 <nathanr88@nycstudents.net>
2025-09-12 07:32:12 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,69 +1,82 @@
+# import turtle
+# from turtle import *
+# t = Turtle()
+# t.shape('turtle')
+# t.speed(3)
+# t.forward(100)
+# t.left(90)
+# t.forward(125)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(125)
+# turtle.clearscreen()
+# import turtle
+# from turtle import *
+# t = Turtle()
+# t.shape('turtle')
+# t.speed(3)
+# t.forward(90)
+# t.left(120)
+# t.forward(90)
+# t.left(120)
+# t.forward(90)
+# turtle.clearscreen()
+# import turtle
+# from turtle import *
+# t = Turtle()
+# t.shape('turtle')
+# t.speed(1000)
+# for i in range(60):
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
+#     t.right(5)
+# turtle.clearscreen()
+# import turtle
+# from turtle import *
+# t = Turtle()
+# t.shape('turtle')
+# t.speed(99999999999999999999999999999999)
+# length=5
+# Turn=90
+# for i in range(60):
+#     forward (length)
+#     left(Turn)
+#     forward (length)
+#     left(Turn)
+#     forward(length)
+#     left(Turn)
+#     forward(length)
+#     left(Turn)
+#     right(5)
+#     length+=5
+# turtle.clearscreen()
 import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-t.speed(3)
-t.forward(100)
-t.left(90)
-t.forward(125)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(125)
-turtle.clearscreen()
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-t.speed(3)
-t.forward(90)
-t.left(120)
-t.forward(90)
-t.left(120)
-t.forward(90)
-turtle.clearscreen()
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-t.speed(1000)
+t.speed(9999999999999)
+Length=5
 for i in range(60):
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.right(5)
-turtle.clearscreen()
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-t.speed(99999999999999999999999999999999)
-length=5
-Turn=90
-for i in range(60):
-    forward (length)
-    left(Turn)
-    forward (length)
-    left(Turn)
-    forward(length)
-    left(Turn)
-    forward(length)
-    left(Turn)
-    right(5)
-    length+=5
-turtle.clearscreen()
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-for i in range(5):
-    forward(100)
-    left(144)
+     forward(Length)
+     left(144)
+     forward(Length)
+     left(144)
+     forward(Length)
+     left(144)
+     forward(Length)
+     left(144)
+     forward(Length)
+     left(144)
+     right(5)
+     Length+=5
+
 
 
 
