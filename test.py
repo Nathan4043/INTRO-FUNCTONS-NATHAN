e61d67d0dd62192dc586195b0ab00503a3fc1d90
Nathan4043 <nathanr88@nycstudents.net>
2025-09-18 09:38:20 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,62 +1,62 @@
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-t.speed(3)
-t.forward(100)
-t.left(90)
-t.forward(125)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(125)
-turtle.clearscreen()
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-t.speed(3)
-t.forward(90)
-t.left(120)
-t.forward(90)
-t.left(120)
-t.forward(90)
-turtle.clearscreen()
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-t.speed(1000)
-for i in range(60):
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.right(5)
-turtle.clearscreen()
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-t.speed(99999999999999999999999999999999)
-length=5
-Turn=90
-for i in range(60):
-    forward (length)
-    left(Turn)
-    forward (length)
-    left(Turn)
-    forward(length)
-    left(Turn)
-    forward(length)
-    left(Turn)
-    right(5)
-    length+=5
-turtle.clearscreen()
+# import turtle
+# from turtle import *
+# t = Turtle()
+# t.shape('turtle')
+# t.speed(3)
+# t.forward(100)
+# t.left(90)
+# t.forward(125)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(125)
+# turtle.clearscreen()
+# import turtle
+# from turtle import *
+# t = Turtle()
+# t.shape('turtle')
+# t.speed(3)
+# t.forward(90)
+# t.left(120)
+# t.forward(90)
+# t.left(120)
+# t.forward(90)
+# turtle.clearscreen()
+# import turtle
+# from turtle import *
+# t = Turtle()
+# t.shape('turtle')
+# t.speed(1000)
+# for i in range(60):
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
+#     t.right(5)
+# turtle.clearscreen()
+# import turtle
+# from turtle import *
+# t = Turtle()
+# t.shape('turtle')
+# t.speed(99999999999999999999999999999999)
+# length=5
+# Turn=90
+# for i in range(60):
+#     forward (length)
+#     left(Turn)
+#     forward (length)
+#     left(Turn)
+#     forward(length)
+#     left(Turn)
+#     forward(length)
+#     left(Turn)
+#     right(5)
+#     length+=5
+# turtle.clearscreen()
 import turtle
 from turtle import *
 t = Turtle()
